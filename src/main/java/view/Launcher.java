34ad8589f3bf283dd4ae0565448bcba586b35a63
katherinew02 <89614140+katherinew02@users.noreply.github.com>
2022-11-20 21:14:13 -0500

    Removed text launcher from view package

--- src/main/java/view/Launcher.java
+++ src/main/java/view/Launcher.java
@@ -1,6 +1,5 @@
 package view;
 
-import java.util.Scanner;
 import javafx.application.Application;
 
 /**
@@ -23,31 +22,7 @@ public class Launcher {
 	 * @param args No program arguments expected.
 	 */
 	public static void main(String[] args) {
-		@SuppressWarnings("resource")
-		Scanner scanner = new Scanner(System.in);
-		boolean cont = true;
-		while (cont) {
-			System.out.println("1 for text, 2 for GUI");
-			
-			if (scanner.hasNextLine()) {
-				String response = scanner.nextLine().strip();
-				if (response.length() != 0) {
-					if (response.charAt(0) == '1') {
-						cont = false;
-						try {
-							TextView.start();
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					} else if (response.charAt(0) == '2') {
-						cont = false;
-						Application.launch(GUI.class, args);
-					} else {
-						System.out.println("Invalid response");
-					}
-				}
-			}
-		}
+		Application.launch(GUI.class, args);
 	}
 
 }
